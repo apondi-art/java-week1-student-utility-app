7606748bc5fc7db80b4a6c9824ccfeac24685e74
apondi-art <aiyanarich64@gmail.com>
2026-02-09 14:40:34 +0300

    add class which diplays menu

---
+++ src/main/java/Menu.java
@@ -0,0 +1,12 @@
+public class Menu {
+
+    public static void showMenu() {
+        System.out.println("\n=== Java Student Utility App ===");
+        System.out.println("1. Add Two Numbers");
+        System.out.println("2. Check EVEN or ODD");
+        System.out.println("3. Print Numbers from 1 to N");
+        System.out.println("4. Display Day of the Week");
+        System.out.println("5. Exit");
+        System.out.print("Choose an option: ");
+    }
+}
--- src/main/java/StudentUtilityApp.java
+++ src/main/java/StudentUtilityApp.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class StudentUtilityApp {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
+        Menu.showMenu();
+
 
 
     }
