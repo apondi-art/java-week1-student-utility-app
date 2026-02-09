15a7fc2866219d41af9f84788ea85c7ac4ffc2d5
apondi-art <aiyanarich64@gmail.com>
2026-02-09 14:51:32 +0300

    add a do while loop to manage show menu and user option

--- src/main/java/StudentUtilityApp.java
+++ src/main/java/StudentUtilityApp.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class StudentUtilityApp {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        Menu.showMenu();
+        int option = 0;
+        do{
+            Menu.showMenu();
+            option = inputScanner.nextInt();
+
+        }while (option != 5);
+
 
 
 
